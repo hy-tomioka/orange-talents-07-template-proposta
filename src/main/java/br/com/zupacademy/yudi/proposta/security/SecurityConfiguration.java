538620e2f02a4c811a7e1c8909e9406b4b771280
yudi <hy-tomioka@github.com>
2021-09-01 14:52:08 -0300

    login via keycloak

---
+++ src/main/java/br/com/zupacademy/yudi/proposta/security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.yudi.proposta.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                .antMatchers(POST, "/api/proposals").hasAuthority("SCOPE_proposta-scope")
+                .anyRequest().authenticated()).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+    }
+}
