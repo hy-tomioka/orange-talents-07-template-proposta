201c711e09c2bf4b82ddd6b2e38133ae5d15ddfb
yudi <hy-tomioka@github.com>
2021-09-03 22:12:56 -0300

    metricas com protheus

--- src/main/java/br/com/zupacademy/yudi/proposta/security/SecurityConfiguration.java
+++ src/main/java/br/com/zupacademy/yudi/proposta/security/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -16,6 +17,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.authorizeRequests(authorizeRequests -> authorizeRequests
                 .antMatchers(POST, "/api/proposals").hasAuthority("SCOPE_proposta-scope")
+                .antMatchers(GET, "/actuator/prometheus").permitAll()
                 .anyRequest().authenticated()).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         http.sessionManagement().sessionCreationPolicy(STATELESS);
     }
